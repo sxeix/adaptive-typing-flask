77a8adef7050c4dcb95c546bded389846ec09e6b
sxeix <sxeixarts@gmail.com>
2022-01-06 21:04:58 +0000

    Introduce basic set of words and random-words route

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import random
 
 
 app = Flask(__name__)
@@ -10,5 +11,14 @@ def test_route():
   basicResponse = dict()
   basicResponse["string"] = "this is a test string"
   return basicResponse
+
+@app.route("/rand-words")
+def get_random_words():
+  words = []
+  with open('words.txt', 'r') as f:
+    words = f.readlines()
+  random.shuffle(words)
+  return " ".join(words[0:40])
+    
 
 app.run()
