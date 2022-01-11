64be8eda86ebf25081457040f4c283abf2c21a1a
sxeix <sxeixarts@gmail.com>
2022-01-11 16:25:39 +0000

    Introduce basic pre-processing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_cors import CORS
 import random
 import logging
+from difflib import SequenceMatcher
+import re
 
 
 app = Flask(__name__)
@@ -31,13 +33,42 @@ def get_random_words():
 @app.route("/test-result", methods=['POST', 'GET'])
 def test_results():
   responseJson = request.get_json()
+  # Need to preprocess the words to remove anomalies
   testWords = responseJson.get('actual')
   typedWords = responseJson.get('typed')
   app.logger.info(testWords)
+  userSet, expectedSet = preprocess_user_results(typedWords, testWords)
   response = dict()
   response['result'] = 'success'
-  response['testWords'] = testWords
-  response['typedWords'] = typedWords
+  # response['typedWords'] = typedWords
+  # response['testWords'] = testWords
+  response['typedWords'] = userSet
+  response['testWords'] = expectedSet
   return response
-  
+
+
+def preprocess_user_results(typed: list, expected: list):
+  expectedFormatted = list()
+  userFormatted = list()
+  for i, tWord in enumerate(typed):
+    typedWord = tWord.lower()
+    expectedWord = expected[i].lower()
+    if typedWord == expectedWord:
+      expectedFormatted.append(expectedWord)
+      userFormatted.append(typedWord)
+    elif typedWord != expectedWord:
+      # caluclate similarity of two strings
+      # only cater if they are more than 70% similar
+      # might need to improve this
+      # also checks that there's no integers in the word
+      similarity = SequenceMatcher(None, typedWord, expectedWord).ratio()
+      containsIntegers = re.search('\d', typedWord)
+      containsSpecialChars = any(char in "!@#$%^&*()-+?_=,<>/" for char in typedWord)
+      if similarity >= 0.7 and not (containsIntegers or containsSpecialChars):
+        app.logger.info('has ran here ' + typedWord)
+        expectedFormatted.append(expectedWord)
+        userFormatted.append(typedWord)
+
+  return userFormatted, expectedFormatted
+
 app.run()
