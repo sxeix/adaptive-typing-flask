ae84ffd07ecf340e286f165487d8e0b03cf21e17
sxeix <sxeixarts@gmail.com>
2022-01-16 18:21:26 +0000

    Refactor for multiple accounts

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 logging.basicConfig(level=logging.DEBUG)
 
-userAgent = Markov("user")
-expectedAgent = Markov("expected")
-
-current_user = 'testuser'
+def get_current_user(user):
+    userAgent = Markov(user)
+    expectedAgent = Markov("expected")
+    data = load_user(user)
+    if not data == None:
+        userHistory, expectedHistory = get_data_lists(data)
+        userAgent.train(userHistory)
+        expectedAgent.train(expectedHistory)
+    return userAgent, expectedAgent
 
 @app.route("/basic")
 def test_route():
@@ -26,8 +31,12 @@ def get_random_words():
     return get_wordset(40, [])
 
 
-@app.route("/tailored-wordset")
+@app.route("/tailored-wordset", methods=["POST", "GET"])
 def get_tailored_words():
+    responseJson = request.get_json()
+    user = responseJson.get("user")
+    userAgent, expectedAgent = get_current_user(user)
+    app.logger.info(f"Generating wordset for {user}")
     error = calculate_error(expectedAgent, userAgent)
     focus_set = find_focus_sets(expectedAgent, error)
     # Should be a good idea to ensure each combo in the focus_set certainly has a % more than 0
@@ -46,7 +55,8 @@ def test_results():
     responseJson = request.get_json()
     testWords = responseJson.get("actual")
     typedWords = responseJson.get("typed")
-
+    user = responseJson.get("user")
+    userAgent, expectedAgent = get_current_user(user)
     userSet, expectedSet = preprocess_user_results(typedWords, testWords)
 
     # app.logger.info(userSet)
@@ -62,23 +72,33 @@ def test_results():
 
     response = dict()
     response["result"] = "success"
+    response["user"] = user
     response["typedWords"] = userSet
     response["testWords"] = expectedSet
     response["focus-sets"] = focus_set
     
-    save_data(current_user, userAgent.get_history(), expectedAgent.get_history())
+    save_data(user, userAgent.get_history(), expectedAgent.get_history())
     
     return response
 
+@app.route("/change-user", methods=["POST", "GET"])
+def change_user():
+    responseJson = request.get_json()
+    user = responseJson.get("user")
+    get_current_user(user)
+    response = dict()
+    response["message"] = f"current user changed to{user}"
+    response["status"] = True
+    
+    return response
+    
+@app.route("/get-users")    
+def get_users():
+    pass
 
 @app.before_first_request
 def startup():
-    data = load_user(current_user)
-    if not data == None:
-        userHistory, expectedHistory = get_data_lists(data)
-        userAgent.train(userHistory)
-        expectedAgent.train(expectedHistory)
-    
+    get_current_user("testuser")
 
 if __name__ == '__main__':
     print(app.before_first_request_funcs)
